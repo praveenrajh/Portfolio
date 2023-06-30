5fdef3d66a06b79bb6728f639d4ed3a4afbb0c63
praveenrajh <ppcupcs@gmail.com>
2023-07-01 01:28:33 +0530

    Header completed

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
+import { FaGithub, FaTwitter } from "react-icons/fa";
 
 const HeaderSocials = () => {
   return (
-    <div className="header__scocials">
+    <div className="header__socials">
       <a href="https://linkedin.com" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank"></a>
-      <a href="https://twitter.com" target="_blank"></a>
+      <a href="https://github.com" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://twitter.com" target="_blank">
+        <FaTwitter />
+      </a>
     </div>
   );
 };
