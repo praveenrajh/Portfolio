05f9d8c646d0714a92e5ed99c1f4206b8d47d435
praveenrajh <ppcupcs@gmail.com>
2023-07-03 21:08:22 +0530

    About changed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,12 +13,13 @@ const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem expedita consequatur porro doloremque dolorum
-            nesciunt eos minima rem cupiditate sint deleniti iusto, dolores
-            neque explicabo est dicta eaque ut. Excepturi. Lorem, ipsum dolor
-            sit amet consectetur adipisicing elit.
+          <p className="about__me-item">
+            An aspiring Full-Stack developer who is learning tech stacks and
+            looking for an opportunity to learn the depth of Software
+            development, Acquired basic web development and designing knowledge;
+            developed a passion for Full-Stack development and eager to apply
+            expertise to create dynamic user interfaces and Application
+            Programming Interface by using various Tech Stacks.
           </p>
         </div>
       </div>
